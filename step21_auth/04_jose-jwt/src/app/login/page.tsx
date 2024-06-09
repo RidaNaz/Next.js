5531b80a5f23ebe50a717a2f43985603f669ce0c
RidaNaz <rnaz3414@gmail.com>
2024-06-09 22:43:26 +0500

    commit by Rida

--- step21_auth/04_jose-jwt/src/app/login/page.tsx
+++ step21_auth/04_jose-jwt/src/app/login/page.tsx
@@ -19,36 +19,43 @@ export default function Login() {
     const data = await response.json().catch(() => null);;
 
     if (response.ok || !data) {
-      localStorage.setItem('token', data.token); // Store the token
-      window.location.href = '/protected'; // Redirect to protected page
+      if (data) {
+        localStorage.setItem('token', data.token); // Store the token
+        window.location.href = '/protected'; // Redirect to protected page
+      } else {
+        console.error('Data is null or undefined.'); // Handle the case where data is null or undefined
+      }
     } else {
       console.error(data.error);
     }
-  };
+  }
 
   return (
-    <form onSubmit={handleLogin}>
+    <form onSubmit={handleLogin} className='my-20 mx-20'>
       <div>
-        <label>
+        <label className='mx-5 '>
           Username:
           <input
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
+            className='text-black mx-5 rounded-md px-3'
           />
         </label>
       </div>
+      <br />
       <div>
-        <label>
+        <label className='mx-5'>
           Password:
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            className='text-black mx-5 rounded-md px-3'
           />
         </label>
       </div>
-      <button type="submit">Login</button>
+      <button className='text-white bg-black font-bold my-5 mx-5 rounded-lg px-3' type="submit">Login</button>
     </form>
   );
 }
